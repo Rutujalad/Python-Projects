91bbae58f30498cd6660eab7b7bd845e4e9ee592
Rutujalad <84790054+Rutujalad@users.noreply.github.com>
2021-05-26 23:52:42 +0530

    Update Guess_Number.py

--- Guess_Number.py
+++ Guess_Number.py
@@ -1,15 +1,20 @@
 
 a=57
+ca=9
 for i in range (9):
     print("Guess the no.:")
     b=int(input())
     if a > b:
         print("No. is less than actual no.")
+        ca=ca-1
     elif b > a:
         print("No.is bigger than actual no.")
+        ca=ca-1
     else:
         print("you guessed it correct")
         break
     print("You left ",8-i,"this much guess")
 
-print("sorry...You lost")
+if ca==0:
+    print("sorry...You lost")
+    
