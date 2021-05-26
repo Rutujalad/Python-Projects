8ea245d78ca35d0a1698e936d0c8d9316bd5d98d
Rutujalad <84790054+Rutujalad@users.noreply.github.com>
2021-05-26 23:34:13 +0530

    Create Guess_Number.py

---
+++ Guess_Number.py
@@ -0,0 +1,15 @@
+
+a=57
+for i in range (9):
+    print("Guess the no.:")
+    b=int(input())
+    if a > b:
+        print("No. is less than actual no.")
+    elif b > a:
+        print("No.is bigger than actual no.")
+    else:
+        print("you guessed it correct")
+        break
+    print("You left ",8-i,"this much guess")
+
+print("sorry...You lost")
